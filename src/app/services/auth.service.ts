f76812ae73a57bd914f765628a500bf04b5aff3c
Alyssa <ajmeller@github.com>
2021-04-16 21:14:13 -0400

    casual coding

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -12,7 +12,20 @@ export class AuthService {
   clientId: string = environment.auth0.clientId;
   codeChallenge: string = '';
 
-  login() {
+  spotifyAuth() {
+    return this.http.post(
+      'https://accounts.spotify.com/api/token',
+      { grant_type: 'client_credentials' },
+      {
+        headers: {
+          Authorization: 'Basic <base64 encoded client_id:client_secret>',
+        },
+      }
+    );
+  }
+  //TODO: configure auth token
+
+  userAuth() {
     return this.http.get(
       `${this.apiUrl}?response_type=code&client_id=${this.clientId}&redirect_uri=http%3A%2F%2Flocalhost&scope=user-follow-modify&state=e21392da45dbf4&code_challenge=${this.codeChallenge}&code_challenge_method=S256`
     );
